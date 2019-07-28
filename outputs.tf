4a19d67c12e2fd94415e4547dcd541968061f504
Hans-Jörg Wieland <mail@wieland.tech>
2019-07-28 14:36:35 +0200

    Latest available Kubernetes version output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "kubernetes_version" {
+  value = data.oci_containerengine_cluster_option.cluster_option.kubernetes_versions[length(data.oci_containerengine_cluster_option.cluster_option.kubernetes_versions) - 1]
+}
+
 output "kubeconfig_path_oci" {
-  value = "${local_file.kubeconfigoci.0.filename}"
+  value = local_file.kubeconfigoci.0.filename
 }
