4c1348ce81c34aacd8444c29cef2454153214f09
Hans-Jörg Wieland <mail@wieland.tech>
2019-08-01 00:10:25 +0200

    Terraform 0.12 syntax

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 provider "oci" {
   alias            = "oci"
   version          = ">=3.28"
-  tenancy_ocid     = "${var.oci_tenancy_ocid}"
-  user_ocid        = "${var.oci_user_ocid}"
-  fingerprint      = "${var.oci_fingerprint}"
-  private_key_path = "${var.oci_private_key_path}"
-  public_key_path  = "${var.oci_public_key_path}"
-  region           = "${var.oci_region}"
+  tenancy_ocid     = var.oci_tenancy_ocid
+  user_ocid        = var.oci_user_ocid
+  fingerprint      = var.oci_fingerprint
+  private_key_path = var.oci_private_key_path
+  public_key_path  = var.oci_public_key_path
+  region           = var.oci_region
 }
